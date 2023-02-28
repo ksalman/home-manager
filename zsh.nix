8a77a9de724b20e246d7ba7195e6bcb9b6eb2210
Kashif Salman <1268871+ksalman@users.noreply.github.com>
2023-02-28 17:12:08 +0000

    Change theme to powerlevel10k
    Theme config `p10k-config/p10k.zsh` needs to be pre-generated

--- zsh.nix
+++ zsh.nix
@@ -1,13 +1,21 @@
-{pkgs, ...}:
+{pkgs, lib, ...}:
 {
   programs.zsh = {
     enable = true;
     history.size = 100000;
-    oh-my-zsh = {
-      enable = true;
-      theme = "robbyrussell";
-      plugins = [ "git" ];
-    };
+
+  plugins = [
+    {
+      name = "powerlevel10k";
+      src = pkgs.zsh-powerlevel10k;
+      file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+    }
+    {
+      name = "powerlevel10k-config";
+      src = lib.cleanSource ./p10k-config;
+      file = "p10k.zsh";
+    }
+  ];
 
   };
 }
