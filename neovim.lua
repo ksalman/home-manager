900da3138e34ff0c894d2e36bdf1cb20410fbfd6
Kashif Salman <1268871+ksalman@users.noreply.github.com>
2023-03-03 20:43:31 -0800

    Convert neovim config to lua

---
+++ neovim.lua
@@ -0,0 +1,49 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.exrc = true
+vim.opt.guicursor = ""
+vim.opt.hidden = true
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+vim.opt.hlsearch = false
+vim.opt.backup = false
+-- https://github.com/neovim/neovim/issues/15720
+vim.opt.undodir = vim.fn.expand('~/.vim/undodir')
+vim.opt.undofile = true
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.showmode = false
+vim.opt.completeopt = {'menuone', 'noinsert', 'noselect'}
+vim.opt.colorcolumn = "88"
+vim.opt.signcolumn = "yes"
+vim.opt.errorbells = false
+vim.wrap = false
+
+vim.g.gruvbox_italics = 1
+vim.cmd.colorscheme('gruvbox')
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fs', function()
+  builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
+
+-- tree-sitter
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+}
--- neovim.nix
+++ neovim.nix
@@ -31,51 +31,9 @@
       commentary
     ];
 
-    extraConfig = ''
-      set tabstop=4
-      set softtabstop=4
-      set shiftwidth=4
-      set expandtab
-      set smartindent
-      set exrc
-      set guicursor=
-      set hidden
-      set relativenumber
-      set number
-      set nowrap
-      set smartcase
-      set ignorecase
-      set nohlsearch
-      set nobackup
-      set undodir=~/.vim/undodir
-      set undofile
-      set incsearch
-      set termguicolors
-      set scrolloff=8
-      set noshowmode
-      set completeopt=menuone,noinsert,noselect
-      set colorcolumn=88
-      set signcolumn=yes
-      set noerrorbells
-
-      let g:gruvbox_italics=1
-      colorscheme gruvbox
-      highlight Normal guibg=none
-
-      let mapleader = " "
-      " Requires vim-telescope
-      nnoremap <leader>ps :lua require('telescope.builtin').grep_string({ search = vim.fn.input("Grep For > ")})<CR>
-      nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<CR>
-      nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<CR>
-      nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<CR>
-      nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<CR>
-      nnoremap <leader>fd :lua require'telescope.builtin'.find_files {cwd = "~/.config",follow=true}<CR>
-
-      " Enable tree-sitter
-      :lua require'nvim-treesitter.configs'.setup { highlight = { enable = true, }, }
-
-      " indent-blankline-nvim
-      :lua require("indent_blankline").setup { show_current_context = true, show_current_context_start = true, }
+    extraLuaConfig = ''
+      ${builtins.readFile ./neovim.lua}
     '';
+
   };
 }
